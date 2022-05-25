e95bd56cd114debe2dbe05695a21bea139f40ce9
artem_g <artemis.gorb.14@gmail.com>
2022-05-25 22:39:23 +0300

    added simple transactions

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,10 @@
 package main
 
 import (
-	"github.com/Thunderbirrd/go_blockchain/inernal/blockchain"
 	cliPackage "github.com/Thunderbirrd/go_blockchain/inernal/cli"
-	"github.com/boltdb/bolt"
-	"log"
 )
 
 func main() {
-	bc := blockchain.NewBlockchain()
-	defer func(DB *bolt.DB) {
-		err := DB.Close()
-		if err != nil {
-			log.Panic(err)
-		}
-	}(bc.DB)
-
-	cli := cliPackage.CLI{BC: bc}
+	cli := cliPackage.CLI{}
 	cli.Run()
 }
